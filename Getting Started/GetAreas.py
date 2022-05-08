bc9f845cbd7058140196f9dd053d24b59d4aef25
Rahul Damodaran <rahuldamodaran@assetfuture.com>
2022-05-08 10:41:51 +1000

    Getting Started

--- Getting Started/GetAreas.py
+++ Getting Started/GetAreas.py
@@ -2,19 +2,7 @@ import requests
 
 response = requests.get(url = "https://api.assetfuture.com/inventory/Areas", 
                         params = {'count': 1,'alldata':False}, 
-                        headers = {'Cache-Control': 'no-cache','Ocp-Apim-Subscription-Key': '1474111d4b3a4982bb41f838b26f9b17'})
-
-areas = response.json()
-
-for area in areas["data"]:
-    print("Name::" + area["name"])
-    print("areaPath::" + area["areaPath"])
-
-#print(areas)                        
-
-response = requests.get(url = "https://api.assetfuture.com/inventory/Areas", 
-                        params = {'count': 1,'alldata':False}, 
-                        headers = {'Cache-Control': 'no-cache','Ocp-Apim-Subscription-Key': '1474111d4b3a4982bb41f838b26f9b17'})
+                        headers = {'Cache-Control': 'no-cache','Ocp-Apim-Subscription-Key': 'SubscriptionKey'})
 
 areas = response.json()
 
